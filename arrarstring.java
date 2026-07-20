7ff2b020f792a907271af95414f089e6e6da76d2
ARUNRAJ-jbg <raj652976@gmail.com>
2026-07-20 14:50:57 +0530

    Create arrarstring.java

---
+++ arrarstring.java
@@ -0,0 +1,10 @@
+public class Main{
+ public static void main(String[]args){
+     int[] marks={10,20,30};
+  String[] Subjects={"tamil","english","maths"};
+  for(int i=0;i<marks.length;i++)
+  {
+      System.out.println(Subjects[i]+"="+marks[i]);
+  }
+ }
+  }
